dcc1b2daea981a51f50df4ec2c3f1cf6f284a5c1
Vladimir <penkinv@gmail.com>
2011-03-12 04:43:02 +0800

    *  Added error checking before page rendering with rails3_before_render plugin

--- lib/generators/mugen/templates/app/controllers/mygengo/comments_controller.rb
+++ lib/generators/mugen/templates/app/controllers/mygengo/comments_controller.rb
@@ -1,8 +1,6 @@
 class Mygengo::CommentsController < Mygengo::MygengoController
   def create                                       
-    mygengo_requests do
-      flag = Mugen::Job.create_comment(params[:id], params[:comment])
-    end
+    flag = Mugen::Job.create_comment(params[:id], params[:comment])
     redirect_to mygengo_job_path(params[:id])
   end
 end
--- lib/generators/mugen/templates/app/controllers/mygengo/jobs_controller.rb
+++ lib/generators/mugen/templates/app/controllers/mygengo/jobs_controller.rb
@@ -4,31 +4,30 @@ class Mygengo::JobsController < Mygengo::MygengoController
   # GET /jobs
   # GET /jobs.xml
   def index
-    mygengo_requests do
-      @job_ids = Mugen::Jobs.all
+    @job_ids = Mugen::Jobs.all
+    if @job_ids
       @jobs = @job_ids.collect {|j| 
         Mugen::Job.find(j['job_id']).merge('comments' => Mugen::Job.comments(j['job_id']))
       }
-    end
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @jobs }
-    end
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @jobs }
+      end
+    else
+      redirect_to mygengo_account_index_path
+    end    
   end
 
   # GET /jobs/1
   # GET /jobs/1.xml
   def show
-    mygengo_requests do
-      @job = Mugen::Job.find(params[:id])
-      @preview = Mugen::Job.preview(@job['job_id'])
-      @comments = Mugen::Job.comments(@job['job_id']) 
-      @job.merge!('comments' => @comments)    
-      @feedback = Mugen::Job.feedback(@job['job_id'])
-      @revisions = Mugen::Job.revisions(@job['job_id'])
-      @revisions = [@revisions] if @revisions.is_a?(Hash)
-    end
+    @job = Mugen::Job.find(params[:id])
+    @preview = Mugen::Job.preview(@job['job_id'])
+    @comments = Mugen::Job.comments(@job['job_id']) 
+    @job.merge!('comments' => @comments)    
+    @feedback = Mugen::Job.feedback(@job['job_id'])
+    @revisions = Mugen::Job.revisions(@job['job_id'])
+    @revisions = [@revisions] if @revisions.is_a?(Hash)
 
     respond_to do |format|
       format.html # show.html.erb
@@ -37,40 +36,32 @@ class Mygengo::JobsController < Mygengo::MygengoController
   end  
   
   def review_form 
-    mygengo_requests do
-      @job = Mugen::Job.find(params[:id])
-      @preview = Mugen::Job.preview(@job['job_id'])
-      @comments = Mugen::Job.comments(@job['job_id']) 
-      @job.merge!('comments' => @comments)        
-      @feedback = Mugen::Job.feedback(@job['job_id'])    
-    end
+    @job = Mugen::Job.find(params[:id])
+    @preview = Mugen::Job.preview(@job['job_id'])
+    @comments = Mugen::Job.comments(@job['job_id']) 
+    @job.merge!('comments' => @comments)        
+    @feedback = Mugen::Job.feedback(@job['job_id'])    
   end
 
   def reject_form  
-    mygengo_requests do
-      @job = Mugen::Job.find(params[:id])
-      @preview = Mugen::Job.preview(@job['job_id'])
-      @comments = Mugen::Job.comments(@job['job_id']) 
-      @job.merge!('comments' => @comments)        
-    end
+    @job = Mugen::Job.find(params[:id])
+    @preview = Mugen::Job.preview(@job['job_id'])
+    @comments = Mugen::Job.comments(@job['job_id']) 
+    @job.merge!('comments' => @comments)        
   end
 
   def revise_form   
-    mygengo_requests do
-      @job = Mugen::Job.find(params[:id])
-      @preview = Mugen::Job.preview(@job['job_id'])
-      @comments = Mugen::Job.comments(@job['job_id']) 
-      @job.merge!('comments' => @comments)        
-    end
+    @job = Mugen::Job.find(params[:id])
+    @preview = Mugen::Job.preview(@job['job_id'])
+    @comments = Mugen::Job.comments(@job['job_id']) 
+    @job.merge!('comments' => @comments)        
   end
 
   # GET /jobs/1
   # GET /jobs/1.xml
   def revision
-    mygengo_requests do
-      @job = Mugen::Job.find(params[:job_id])    
-      @revision = Mugen::Job.revision(params[:job_id], params[:id])
-    end
+    @job = Mugen::Job.find(params[:job_id])    
+    @revision = Mugen::Job.revision(params[:job_id], params[:id])
     
     respond_to do |format|
       format.html {}
@@ -81,14 +72,12 @@ class Mygengo::JobsController < Mygengo::MygengoController
   # GET /jobs/new
   # GET /jobs/new.xml
   def new
-    mygengo_requests do
-      @job = Mugen::Job                                                    
-      @languages = Mugen::Service.languages
-      @language_pairs = Mugen::Service.language_pairs
-      @language_src = @languages.collect { |lp| [ lp["language"], lp['lc'] ] }.uniq.sort
-      @language_tgt = @languages.collect { |lp| [ lp["language"], lp['lc']  ] }.uniq.sort
-    end
-    
+    @job = Mugen::Job                                                    
+    @languages = Mugen::Service.languages
+    @language_pairs = Mugen::Service.language_pairs
+    @language_src = @languages.collect { |lp| [ lp["language"], lp['lc'] ] }.uniq.sort
+    @language_tgt = @languages.collect { |lp| [ lp["language"], lp['lc']  ] }.uniq.sort
+
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @job }
@@ -97,9 +86,7 @@ class Mygengo::JobsController < Mygengo::MygengoController
 
   # GET /jobs/1/edit
   def edit         
-    mygengo_requests do
-      @job = Mugen::Job.find(params[:id])
-    end
+    @job = Mugen::Job.find(params[:id])
   end
 
   # POST /jobs
@@ -110,50 +97,39 @@ class Mygengo::JobsController < Mygengo::MygengoController
        !params[:job]['callback_url'].match(URL_REGEXP)
       params[:job].delete('callback_url') 
     end
-    mygengo_requests do
-      flag = Mugen::Job.create(params[:job])    
-    end
+    flag = Mugen::Job.create(params[:job])    
+
     redirect_to mygengo_jobs_path
   end
 
   # PUT /jobs/1
   # PUT /jobs/1.xml
   def update
-    mygengo_requests do
-      @job = Mugen::Job.update(params[:id])
-    end
+    @job = Mugen::Job.update(params[:id])
     
     redirect_to mygengo_jobs_path
   end             
   
   def purchase 
-    mygengo_requests do
-      @job = Mugen::Job.purchase(params[:id])
-    end
+    @job = Mugen::Job.purchase(params[:id])
     
     redirect_to mygengo_jobs_path
   end
 
   def approve  
-    mygengo_requests do
-      @job = Mugen::Job.approve(params[:id])
-    end
+    @job = Mugen::Job.approve(params[:id])
     
     redirect_to mygengo_jobs_path
   end
 
   def revise
-    mygengo_requests do
-      @job = Mugen::Job.revise(params[:id], params[:job])
-    end
+    @job = Mugen::Job.revise(params[:id], params[:job])
     
     redirect_to mygengo_jobs_path
   end
 
   def reject  
-    mygengo_requests do
-      @job = Mugen::Job.reject(params[:id], params[:job])
-    end
+    @job = Mugen::Job.reject(params[:id], params[:job])
     
     redirect_to mygengo_jobs_path
   end
@@ -161,9 +137,7 @@ class Mygengo::JobsController < Mygengo::MygengoController
   # DELETE /jobs/1
   # DELETE /jobs/1.xml
   def destroy    
-    mygengo_requests do
-      @job = Mugen::Job.delete(params[:id])
-    end
+    @job = Mugen::Job.delete(params[:id])
     
     respond_to do |format|
       format.html { redirect_to(mygengo_jobs_url) }
